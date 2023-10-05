dc6731ff1b487dcc844914192235e7245d78c87c
MohsinSheikh1 <sheikhmohsin181@gmail.com>
2023-10-06 01:24:30 +0500

    new

--- utils/generatePDF.js
+++ utils/generatePDF.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 async function generatePDF(html) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true });
 
   const page = await browser.newPage();
 
