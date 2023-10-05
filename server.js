edbd81e0e0fcf252f523436bdee602f01d290183
MohsinSheikh1 <sheikhmohsin181@gmail.com>
2023-10-05 13:04:01 +0500

    new

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const app = require("./app");
 const port = process.env.PORT || 5000;
-const db = require("./db");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+try {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+      app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+      });
+    });
+} catch (error) {
+  console.log("Could not connect to MongoDB");
+}
+
+// const db = mongoose.connection;
